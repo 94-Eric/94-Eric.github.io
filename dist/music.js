42057efa258640d58e6f7de933c06b98142ddb5b
94Eric <you@example.com git config --global user.name Your>
2023-06-18 22:50:42 +0800

    Site updated: 2023-06-18 22:50:42

--- dist/music.js
+++ dist/music.js
@@ -9,18 +9,6 @@ const ap = new APlayer({
         url: 'http://lc-q8NLFAyg.cn-n1.lcfile.com/Ch5OsHWO32ntrQxko9mLvlc5GflGIj5B/%E6%9E%97%E4%B8%80%20-%20%E9%BB%91%E5%A4%9C%E5%B0%86%E5%B0%BD%20%28%E7%89%87%E6%AE%B5%29.mp3
 ',
         cover: 'http://lc-q8NLFAyg.cn-n1.lcfile.com/cUOtCf89GHnhmO2ayi0UveEx6Bl4OGpo/%E6%8D%95%E8%8E%B712.png',
-      },
-      {
-        name: '9420',
-        artist: '麦小兜',
-        url: 'http://up.mcyt.net/?down/45967.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
-      },
-      {
-        name: '风筝误',
-        artist: '刘珂矣',
-        url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
       }
     ]
 });
